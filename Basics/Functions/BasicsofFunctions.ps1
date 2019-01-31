640f17cdff75db58b724c2c2b53bf947a2853c86
Nathan <nathan@abourbih.com>
2019-01-31 11:15:44 -0500

    Cleaned up disk space scripts and added function examples.
    Removed duplicate code and added new function examples.

---
+++ Basics/Functions/BasicsofFunctions.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+Basics of Functions
+.DESCRIPTION
+Several examples showing the basics of functions and how to use them.
+.NOTES  
+File Name  : BasicssofVFunctions.ps1
+Author     : Nathan Abourbih - nathan@abourbih.com 
+#>
+
+function Get-Greeting
+{ 
+    return "Hello, sunshine!" 
+}
+
+Write-Host "Your greeting is: $(Get-Greeting)"
+
+function Get-RandomGreeting
+{ 
+    $greetings = @(
+        "Hi!",
+        "Howdy, partner!",
+        "Peek-a-boo!",
+        "What's up?",
+        "Hey there, freshman!",
+        "Hi, mister!",
+        "I come in peace!",
+        "Put that cookie down!",
+        "Welcome!"
+    )
+
+    return $greetings | Get-Random
+}
+
+Write-Host "Your Random greeting is: $(Get-RandomGreeting)"
--- Basics/GetDiskSpaceUsingCustomLines.ps1
+++ Basics/GetDiskSpaceUsingCustomLines.ps1
@@ -8,6 +8,8 @@ File Name  : GetDiskSpaceUsingCustomLines.ps1
 Author     : Nathan Abourbih - nathan@abourbih.com 
 #>
 
+$disks = Get-WmiObject Win32_LogicalDisk
+
 $lineWidths = "{0,-12} {1,-12} {2,-12} {3,-12} {4,-12} {5,-12} {6,-12}"
 
 write-output $($lineWidths -f "Letter","Name","Size (GB)","Size (TB)","Free (GB)","Free (TB)","Free (%)")
@@ -23,59 +25,3 @@ foreach ($disk in $disks)
   ([math]::Round($disk.FreeSpace / $disk.Size, 2) * 100)
   )
 }
-
-
-<# =======================================================
-   Example 3
-    - This example uses a property list with a label and
-    expression as well as a variety of html styles,
-    headers, footers, and even bootstrap to customize
-    the output of the ConvertTo-Html cmdlet and save the
-    result to a file.
-# =======================================================#>
-
-Write-Host "`nGenerating Disk Space Report..."  -ForegroundColor Blue
-
-$reportSaveLocation = "C:\Users\nate\Desktop\DisksSpaceReport.html"
-
-$properties = @(
-    @{ Label = "Letter"; Expression = {$_.DeviceID}},
-    @{ Label = "Name"; Expression = {$_.VolumeName}},
-    @{ Label = "Size (GB)"; Expression = {[math]::Round($_.Size / 1GB, 2) }; },
-    @{ Label = "Size (TB)"; Expression = {[math]::Round($_.Size / 1TB, 2) }; },
-    @{ Label = "Free (GB)"; Expression = {[math]::Round($_.FreeSpace / 1GB, 2) }; },
-    @{ Label = "Free (TB)"; Expression = {[math]::Round($_.FreeSpace / 1TB, 2) };},
-    @{ Label = "Free (%)"; Expression = {([math]::Round($_.FreeSpace / $_.Size, 2) * 100) }; }
-)
-
-$styles = @"
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<style>
-body { padding: 2em; }
-</style>
-<script>
-  `$(document ).ready(function() {
-    `$('table').addClass('table').addClass('table-striped').addClass('table-hover').addClass('table-bordered');
-  });
-</script>
-"@
-
-$formattedDate = Get-Date -Format "D";
-$formattedTime = Get-Date -Format "T";
-
-$top = @"
-<div class="jumbotron" style="padding: 2em;">
-<h1 class="display-4">Disk Space Report</h1>
-<p class="lead">For Computer: $($env:computername)</p>
-<hr class="my-4">
-<p>This report outlines the current state of the logical disks for $($env:computername) on $formattedDate at $formattedTime.</p>
-<a class="btn btn-primary btn-lg" href="https://moodle.cambriancollege.ca/course/view.php?id=13942" role="button">Learn more</a>
-</div>
-"@
-
-$bottom = @"
-<p>This report was generated $(Get-Date) by $($env:UserName).</p>
-"@
-
-Get-WmiObject Win32_LogicalDisk | ConvertTo-Html -Property $properties -Head $styles -PreContent $top -PostContent $bottom | Out-File -FilePath $reportSaveLocation
